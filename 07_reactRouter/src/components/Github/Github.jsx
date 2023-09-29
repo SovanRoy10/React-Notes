7893e0194bc54a399066a539de56f2fa4fd23e4c
Sovan Roy <roysovan00@gmail.com>
2023-09-29 20:10:21 +0530

    commit

---
+++ 07_reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+// import  { useEffect, useState } from 'react'
+import { useLoaderData } from "react-router-dom"
+export default function Github() {
+
+    // const [data, setData] = useState({})
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/SovanRoy10')
+    //         .then(Response => Response.json())
+    //         .then((data) => {
+    //             setData(data)
+    //         })
+    // }, [])
+
+    const data = useLoaderData()
+    return (
+        <>
+            <div className='text-center m-4 bg-gray-600 text-white text-3xl p-4'>Github followers : {data.followers}</div>
+            <img src={data.avatar_url} alt="avatar" className='text-center mx-auto rounded-lg'/>
+        </>
+    )
+}
+
+export const githubInfoLoader = async ()=>{
+    const response = await fetch('https://api.github.com/users/SovanRoy10')
+    return response.json()
+
+}
